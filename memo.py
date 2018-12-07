dc309623dbc601927e3c4cdfd8622e968240efe3
emceha <em.chrzanowski@gmail.com>
2018-12-07 21:17:10 +0100

    less bs

--- memo.py
+++ memo.py
@@ -4,41 +4,40 @@ import sys
 import random
 import pygame
 import time
+from pygame.locals import *
 
 
 class Monster():
-    def __init__(self, face, found):
+    def __init__(self, face):
         self.face = face
-        self.found = found
+        self.found = False
 
 
 pygame.init()
-clock = pygame.time.Clock()
 
-blank = pygame.image.load("./img/blank_a.png")
+blank = pygame.image.load("./img/blank.png")
 bgimg = pygame.image.load("./img/background.jpg")
 screen = pygame.display.set_mode((600, 600))
-
-DOT = "\N{MIDDLE DOT}"
 
 monsters = []
 for n in range(18):
     face = pygame.image.load(f"./img/{n}.png")
-    monsters.append(Monster(face, False))
-    monsters.append(Monster(face, False))
+    monsters.append(Monster(face))
+    monsters.append(Monster(face))
 
 random.shuffle(monsters)
 for n, m in enumerate(monsters):
     m.x = (n % 6) * 100 + 2
     m.y = (n // 6) * 100 + 2
+    m.r = pygame.Rect(m.x + 15, m.y + 15, 70, 70)
 
-clicks = 0
 running = True
 gameover = False
 warmup = True
 
 m2check = set()
-gameon = hold = time.time()
+hold = time.time()
+clock = pygame.time.Clock()
 
 while running:
     clock.tick(50)
@@ -55,16 +54,8 @@ while running:
         else:
             screen.blit(blank, (m.x, m.y))
 
-    if not warmup:
-        if not gameover:
-            tm = int(time.time() - gameon)
-            st = "{:02d}:{:02d}".format(tm // 60, tm % 60)
-        pygame.display.set_caption(f"Time {st}  {DOT}  Clicks {clicks:03d}")
-    else:
-        sek = int(9 - (time.time() - gameon))
-        pygame.display.set_caption(f"Start In   {sek} s")
-        if sek < 1:
-            gameon = time.time()
+    if warmup:
+        if time.time() - hold > 9:
             warmup = False
 
     if len(m2check) > 1 and time.time() - hold > 0.6:
@@ -75,15 +66,15 @@ while running:
                 gameover = True
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if not gameover and not warmup:
-                if event.button == 1:
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if not warmup and len(m2check) < 2:
                     x, y = pygame.mouse.get_pos()
                     m = monsters[(y // 100) * 6 + x // 100]
-                    if not m.found and m not in m2check:
-                        if len(m2check) < 2:
-                            m2check.add(m)
-                            hold = time.time()
-                            clicks += 1
+                    if m.found or m in m2check:
+                        break
+                    if m.r.collidepoint(x, y):
+                        m2check.add(m)
+                        hold = time.time()
